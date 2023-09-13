cdd21bb9e318adf0e67be13be62410e45215889d
z3301 <scoopnisker@gmail.com>
2023-09-13 09:35:30 -0400

    step 1

---
+++ ForecastViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ForecastViewController.swift
+//  CloudCast
+//
+//  Created by Daniel Zimmerman on 9/13/23.
+//
+
+// Import the UIKit framework
+// You're almost always going to need this when your referencing UI elements in your file
+import UIKit
+
+// Class declaration, including the name of the class and its subclass (UIViewController)
+class ForecastViewController: UIViewController {
+
+    // Function override for the view controller
+    // This is fired when the view has finished loading for the first time
+    override func viewDidLoad() {
+        // Some functions require you to call the super class implementation
+        // Always read the online documentation to know if you need to
+        super.viewDidLoad()
+    }
+}
